7e759c904b7168c73ff8556a8c92284f861b290b
kellyhirano <hirano@cs.stanford.edu>
2020-08-19 19:43:17 -0700

    Added AQI. Refactored data storage to allow multiple mqtt subjects

--- weather.py
+++ weather.py
@@ -7,12 +7,7 @@ import paho.mqtt.client as mqtt
 import fourletterphat as flp
 
 # Global for data storage
-g_outside_temp = 0
-g_outside_temp_change = 0
-g_outside_24h_temp_change = 0
-g_last_day_rain = 0
-g_rain_rate = 0
-g_wind_gust = 0
+g_mqtt_data = {}
 
 
 def on_connect(client, userdata, flags, rc):
@@ -22,6 +17,7 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe([("weewx/sensor", 0),
+                      ("purpleair/sensor", 0),
                       ("awair/Family Room/sensor", 0),
                       ("awair/Master Bedroom/sensor", 0),
                       ("awair/Living Room/sensor", 0)])
@@ -29,22 +25,12 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     """The callback for when a PUBLISH message is received from the server."""
-    global g_outside_temp
-    global g_outside_temp_change
-    global g_outside_24h_temp_change
-    global g_last_day_rain
-    global g_rain_rate
-    global g_wind_gust
+    global g_mqtt_data
 
     print(msg.topic+" -> "+str(msg.payload.decode('UTF-8')))
-    weather_data = json.loads(str(msg.payload.decode('UTF-8')))
+    message_data = json.loads(str(msg.payload.decode('UTF-8')))
 
-    g_outside_temp = weather_data['outdoor_temperature']
-    g_outside_temp_change = weather_data['outdoor_temp_change']
-    g_outside_24h_temp_change = weather_data['outdoor_24h_temp_change']
-    g_last_day_rain = weather_data['last_day_rain']
-    g_rain_rate = weather_data['rain_rate']
-    g_wind_gust = weather_data['wind_gust']
+    g_mqtt_data[msg.topic] = message_data
 
     # Flash the rightmost decimal to show receipt of a message
     flp.set_decimal(3, True)
@@ -72,24 +58,50 @@ client.on_message = on_message
 
 client.connect(mqtt_host, mqtt_host_port, 60)
 
+
 while(1):
+    if ('weewx/sensor' not in g_mqtt_data):
+        time.sleep(5)
+        client.loop()
+        continue
+
+    temp = g_mqtt_data['weewx/sensor']['outdoor_temperature']
+    temp_change = g_mqtt_data['weewx/sensor']['outdoor_temp_change']
+    temp_change_24h = g_mqtt_data['weewx/sensor']['outdoor_24h_temp_change']
+    rain_rate = g_mqtt_data['weewx/sensor']['rain_rate']
+    wind_gust = g_mqtt_data['weewx/sensor']['wind_gust']
+
+    aqi = 0
+    if ('purpleair/sensor' in g_mqtt_data):
+        aqi = g_mqtt_data['purpleair/sensor']['st_aqi']
+
     current_hour = int(time.strftime("%H", time.localtime()))
+    if (current_hour >= 7 and current_hour <= 23):
 
-    if (current_hour >= 7 and current_hour <= 23 and g_outside_temp):
+        if (aqi >= 100):
+            print('AQI: {}'.format(aqi))
+            flp.clear()
+            flp.print_str('AWI')
+            flp.show()
+            time.sleep(.5)
+            flp.clear()
+            flp.print_number_str(aqi)
+            flp.show()
+            time.sleep(2)
 
-        if (g_wind_gust >= 10):
-            print('GUST: {}'.format(g_wind_gust))
+        if (wind_gust >= 10):
+            print('GUST: {}'.format(wind_gust))
             flp.clear()
             flp.print_str('GUST')
             flp.show()
             time.sleep(.5)
             flp.clear()
-            flp.print_number_str(g_wind_gust)
+            flp.print_number_str(wind_gust)
             flp.show()
             time.sleep(2)
 
-        if (g_rain_rate):
-            print('RAIN: {:.2f}h'.format(g_rain_rate))
+        if (rain_rate > 0):
+            print('RAIN: {:.2f}h'.format(rain_rate))
             flp.clear()
             flp.print_str('RAIN')
             flp.show()
@@ -99,7 +111,7 @@ while(1):
             flp.show()
             time.sleep(.5)
             flp.clear()
-            flp.print_float(g_rain_rate, decimal_digits=2)
+            flp.print_float(rain_rate, decimal_digits=2)
             flp.show()
             time.sleep(2)
 
@@ -108,23 +120,15 @@ while(1):
         flp.show()
         time.sleep(.5)
         flp.clear()
-        flp.print_float(g_outside_temp, decimal_digits=1)
+        flp.print_float(temp, decimal_digits=1)
         flp.show()
         time.sleep(2)
         flp.clear()
-        flp.print_float(g_outside_temp_change, decimal_digits=1)
+        flp.print_float(temp_change, decimal_digits=1)
         flp.show()
         time.sleep(2)
         flp.clear()
-        flp.print_float(g_outside_temp, decimal_digits=1)
-        flp.show()
-        time.sleep(2)
-        flp.clear()
-        flp.print_str('24H')
-        flp.show()
-        time.sleep(.5)
-        flp.clear()
-        flp.print_float(g_outside_24h_temp_change, decimal_digits=1)
+        flp.print_float(temp, decimal_digits=1)
         flp.show()
         time.sleep(2)
         flp.clear()
@@ -132,10 +136,16 @@ while(1):
         flp.show()
         time.sleep(.5)
         flp.clear()
-        flp.print_float(g_outside_temp, decimal_digits=1)
+        flp.print_float(temp_change_24h, decimal_digits=1)
         flp.show()
         time.sleep(2)
         flp.clear()
+        flp.print_str('24H')
+        flp.show()
+        time.sleep(.5)
+        flp.clear()
+        flp.print_float(temp, decimal_digits=1)
+        flp.show()
 
     # Give the main display a rest at night and show a blinky pattern
     else:
